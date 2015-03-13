ccce93e1470acdbed512bc22cd8332607cbdd3f7
Daniel Tsui <tsui.daniel@gmail.com>
2015-03-13 17:38:00 +1100

    bar

--- 01-javascript/html-preview/bar.js
+++ 01-javascript/html-preview/bar.js
@@ -1,10 +1,20 @@
 function doSomething() {
   var userAge = document.getElementById('userAge').value;
-  console.log('userAge: ', userAge);
+  var titleElem = document.getElementById('title');
 
   // to convert string to integer use parseInt()
+  if (parseInt(userAge, 10) >= 18) {
+    titleElem.innerHTML = 'yay';
+  } else {
+    titleElem.innerHTML = 'boo';
+  }
+}
+
+function doSomthingElse() {
+  console.log('do something else');
 }
 
 // hey document can you get me the element with id 'btnEnter'
 var btnEnter = document.getElementById('btnEnter');
 btnEnter.addEventListener('click', doSomething);
+
