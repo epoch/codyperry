c2e1d94527b4a095f3a86c36a5e459ab178f9c02
Daniel Tsui <tsui.daniel@gmail.com>
2015-03-16 15:07:15 +1100

    hotdogs sandwiches and toasts

---
+++ 02-web/callbacks/cats.js
@@ -0,0 +1,14 @@
+var movePixels = 10;
+var delayMs = 50;
+var catTimer = null;
+function catWalk() {
+  var img = document.getElementsByTagName('img')[0];
+  var currentLeft = parseInt(img.style.left);
+  img.style.left = (currentLeft + movePixels) + 'px';
+  if (currentLeft > (window.innerWidth-img.width)) {
+    img.style.left = '0px';
+  }
+}
+function startCatWalk() {
+  catTimer = window.setInterval(catWalk, delayMs);
+}
---
+++ 02-web/callbacks/sync.rb
@@ -0,0 +1,5 @@
+puts "hello world"
+
+sleep 5
+
+puts "hello again"
