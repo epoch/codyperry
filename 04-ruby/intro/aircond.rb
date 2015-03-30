8c16b46e8086125b38b644d62069c863456f40c1
Daniel Tsui <tsui.daniel@gmail.com>
2015-03-30 16:58:20 +1100

    for matt and nick

--- 04-ruby/intro/aircond.rb
+++ 04-ruby/intro/aircond.rb
@@ -1,26 +1,33 @@
-print "What is the current temp? "
-current_temp = gets.chomp.to_i
 
-print "Is the air con operational (y/n) "
-# true is Y, or y, false otherwise
-working_air_con = (gets.chomp.downcase == 'y')
+  print "What is the current temp? " 
+  current_temp = gets.chomp.to_i 
+  print "Is the air con operational (y/n) "
+  # true is Y, or y, false otherwise
+  working_air_con = (gets.chomp.downcase == 'y')  
+  print "what is the desired temp? "
+  desired_temp = gets.chomp.to_i
 
-print "what is the desired temp? "
-desired_temp = gets.chomp.to_i
 
+def air_con(working_air_con, current_temp, desired_temp)
+  if working_air_con
 
-if working_air_con
+    if current_temp > desired_temp
+      puts "Turn on the air con please"
+    end
 
-  if current_temp > desired_temp
-    puts "Turn on the air con please"
+  else
+    # no working air con
+    if current_temp > desired_temp
+      puts "fix the air con, its hot!"
+    elsif current_temp < desired_temp
+      puts "fix the air con when you have time. its cool"
+    end
+    
   end
-
-else
-  # no working air con
-  if current_temp > desired_temp
-    puts "fix the air con, its hot!"
-  elsif current_temp < desired_temp
-    puts "fix the air con when you have time. its cool"
-  end
-  
 end
+
+air_con(working_air_con, current_temp, desired_temp)
+
+
+
+
