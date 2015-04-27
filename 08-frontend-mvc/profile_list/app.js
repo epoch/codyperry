e27bf5f352409ebc7d6c25a8221845e9f17ad1be
Daniel Tsui <tsui.daniel@gmail.com>
2015-04-27 17:24:33 +1000

    backbone view example

--- 08-frontend-mvc/profile_list/app.js
+++ 08-frontend-mvc/profile_list/app.js
@@ -14,11 +14,55 @@ var profiles = [{
   image_url: 'https://placekitten.com/g/300/200'
 }];
 
-var profileItem = _.template($("#profile-item-template").html());
+// var profileItem = _.template($("#profile-item-template").html());
 
+// _.each(profiles, function(profile, index) {
+//   var html = profileItem(profile);
+//   $('#container').append(html);  
+// });
 
-_.each(profiles, function(profile, index) {
-  var html = profileItem(profile);
-  console.log(html);
-  $('#container').append(html);  
+// $('.profile-item').on('click', function() {
+//   console.log('360 rotation');
+//   console.log("hello " + $(this).find("h2").html());
+// });
+
+var ProfileItemView = Backbone.View.extend({
+  events: {
+    "click h2": "itemClicked"
+  },
+  itemClicked: function() {
+    console.log('item clicked');
+  },
+  render: function() {
+    var template = _.template( $("#profile-item-template").html() );
+    this.$el.html(template(this.model));
+  }
 });
+
+_.each(profiles, function(profile) {
+  var view = new ProfileItemView({ model: profile});
+  view.render();
+  $('#container').append(view.el);
+})
+
+
+
+
+
+
+
+
+
+
+// var MenuView = Backbone.View.extend({
+//   el: '#menu',
+//   events: {
+//     'click': 'menuClick'
+//   },
+
+//   menuClick: function() {
+//     console.log('menu clicked');
+//   }
+// });
+
+// var menuView = new MenuView();
